e3d42d9f8e275fe459a279db7c1d1d84bc25832a
Gerson <gercentu777@gmail.com>
2023-11-17 23:49:53 -0300

    Corrigiendo ruta

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:5432/api/";
-const BASE_URL = "https://backvideogame.onrender.com";
+const BASE_URL = "https://backvideogame.onrender.com/api";
 const VIDEOGAMES_URL = BASE_URL + "videogames/";
 const GENRES_URL = BASE_URL + "genres/";
 
